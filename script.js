2feffdd6319b794ef703bf999e716b19caee8bdc
Julien Baouz <baouzjulien@gmail.com>
2025-11-11 10:50:34 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+const tabs = document.querySelectorAll('[role="tab"]');
+const panels = document.querySelectorAll('[role="tabpanel"]');
 
+tabs.forEach(tab => {
+  tab.addEventListener("click", () => {
+    tabs.forEach(t => t.setAttribute("aria-selected", "false"));
+    panels.forEach(p => p.hidden = true);
+    tab.setAttribute("aria-selected", "true");
+    let associatedPanel = tab.getAttribute("aria-controls");
+    let panel = document.getElementById(associatedPanel);
+    panel.removeAttribute("hidden");
+  });
+});
